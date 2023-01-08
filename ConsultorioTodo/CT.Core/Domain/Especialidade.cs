e397446d9aa23902039fef1d15266065aaa2f670
Juliolimahen <juliolima_henrique@outlook.com>
2023-01-08 20:28:37 -0300

    Novas entidades

---
+++ ConsultorioTodo/CT.Core/Domain/Especialidade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CT.Core.Domain
+{
+    public class Especialidade
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public ICollection<Medico> Medicos { get; set; }
+    }
+}
---
+++ ConsultorioTodo/CT.Core/Domain/Medico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CT.Core.Domain
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Crm { get; set; }
+        public ICollection<Especialidade> Especialidades { get; set; }
+
+        public Medico()
+        {
+            Especialidades = new HashSet<Especialidade>();
+        }
+    }
+}
